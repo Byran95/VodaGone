e6c1e79ff9dae66721bbce167619b5d3af82f7c8
Anders Egberts <AndersEgberts@live.nl>
2016-10-12 13:39:38 +0200

    Restored IAbonnement

--- src/DomainApplication/IAbonnement.java
+++ src/DomainApplication/IAbonnement.java
@@ -1,7 +1,13 @@
 package DomainApplication;
 
 /**
- * Created by Bryan on 12-10-2016.
+ * Created by Anders Egberts on 12/10/2016.
  */
 public interface IAbonnement {
+
+    void verdubbel();
+    void deelMet( IAbonnee abonnee );
+    void verleng( AbonnementSoort periode );
+    void zegOp();
+    float berekendMaandPrijs();
 }
