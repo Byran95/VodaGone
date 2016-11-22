23738d40eb1149a7d6b9cef8245506e2e17a29ae
Anders Egberts <AndersEgberts@live.nl>
2016-11-22 15:15:28 +0100

    Created the beginning of a rest service.

---
+++ src/main/java/Jersey/AbonneeResource.java
@@ -0,0 +1,7 @@
+package Jersey;
+
+/**
+ * Created by Anders Egberts on 22/11/2016.
+ */
+public class AbonneeResource {
+}
---
+++ src/main/java/Jersey/DienstResource.java
@@ -0,0 +1,22 @@
+package Jersey;
+
+import DomainApplication.IDienst;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Anders Egberts on 22/11/2016.
+ */
+public class DienstResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<IDienst> getAll() {
+        //TODO: Implement get
+        return new ArrayList<>();
+    }
+}
