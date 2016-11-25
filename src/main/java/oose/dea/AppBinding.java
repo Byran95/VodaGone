4ed0fcd03f364392d574ec83479e8153ea82ffab
Anders Egberts <AndersEgberts@live.nl>
2016-11-25 16:20:26 +0100

    Removed example classes.

--- src/main/java/oose/dea/AppBinding.java
+++ src/main/java/oose/dea/AppBinding.java
@@ -2,8 +2,6 @@ package oose.dea;
 
 import com.google.inject.servlet.ServletModule;
 import oose.dea.controller.*;
-import oose.dea.dao.FakeItemDAO;
-import oose.dea.dao.ItemDAO;
 import oose.dea.domain.IAbonneeAccess;
 import oose.dea.domain.IAbonnementAccess;
 import oose.dea.domain.IDienstAccess;
@@ -13,11 +11,9 @@ import oose.dea.domain.MySQLDataAccess.DienstDAOMySQL;
 import oose.dea.services.IAbonneeService;
 import oose.dea.services.IAbonnementService;
 import oose.dea.services.IDienstService;
-import oose.dea.services.ItemService;
 import oose.dea.services.local.AbonneeService;
 import oose.dea.services.local.AbonnementService;
 import oose.dea.services.local.DienstService;
-import oose.dea.services.local.LocalItemService;
 import oose.dea.services.rest.AbonneeRestService;
 import oose.dea.services.rest.AbonnementRestService;
 import oose.dea.services.rest.DienstRestService;
@@ -29,7 +25,7 @@ public class AppBinding extends ServletModule {
     @Override
     protected void configureServlets() {
         super.configureServlets();
-        serve("/viewItems").with(ItemsView.class);
+
         //Servlets
         serve("/login").with(LoginServlet.class);
         serve("/abonnementen").with(AbonnementServlet.class);
@@ -41,10 +37,8 @@ public class AppBinding extends ServletModule {
         serve("/tryService").with(ProbeerDienstServlet.class);
         serve("/register").with(RegisterAbonneeServlet.class);
 
-        bind(ItemService.class).to(LocalItemService.class);
-        bind(ItemDAO.class).to(FakeItemDAO.class);
         //Bind services
-        boolean useRest = true;
+        boolean useRest = true; //TODO: Replace with settings-parameter
         if ( useRest ) {
             bind(IAbonnementService.class).to(AbonnementRestService.class);
             bind(IAbonneeService.class).to(AbonneeRestService.class);
